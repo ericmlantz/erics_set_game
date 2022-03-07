2473dd637ff5cbf5b7f88cc93c846c1a6c87ef21
Eric Lantz <ericmlantz@gmail.com>
2022-03-07 16:12:50 -0500

    Created the card class and it's 4 parameter properties. Created a deck array that cycles through all the properties and makes a unique card object (from the card class) that creates a full  81 card deck of unique cards with their individual properties as key/value pairs

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ colors = ['red', 'purple', 'green']
 numbers = ['one', 'two', 'three']
 shading = ['solid', 'striped', 'outlined']
 
+class Card {
+  constructor(shape, color, shade, number) {
+    this.shape = shape
+    this.color = color
+    this.shade = shade
+    this.number = number
+  }
+}
+
+const deck = []
 for (let shapesIndex = 0; shapesIndex < shapes.length; shapesIndex++) {
   for (let colorsIndex = 0; colorsIndex < colors.length; colorsIndex++) {
     for (let numbersIndex = 0; numbersIndex < numbers.length; numbersIndex++) {
@@ -11,16 +21,15 @@ for (let shapesIndex = 0; shapesIndex < shapes.length; shapesIndex++) {
         shadingIndex < shading.length;
         shadingIndex++
       ) {
-        console.log(
-          shapes[shapesIndex] +
-            ' ' +
-            colors[colorsIndex] +
-            ' ' +
-            numbers[numbersIndex] +
-            ' ' +
-            shading[shadingIndex]
+        let card = new Card(
+          shapes[shapesIndex],
+          colors[colorsIndex],
+          numbers[numbersIndex],
+          shading[shadingIndex]
         )
+        deck.push(card)
       }
     }
   }
 }
+console.log(deck)
