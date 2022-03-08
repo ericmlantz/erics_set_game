3d010850e312af0d304f62b78fc3954f9a82ab44
Eric Lantz <ericmlantz@gmail.com>
2022-03-08 16:58:28 -0500

    Created a function that sets the properties of the cards. No transfer to next card though yet, that is next step. For loop for moving throuhg the 12 and adding cards to them.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //Card Properties
-shapes = ['square', 'circle', 'plus']
-colors = ['green', 'blue', 'purple']
+shapes = ['square', 'circle', 'semiCircle']
+colors = ['Green', 'Blue', 'Purple']
 numbers = ['one', 'two', 'three']
-shading = ['solid', 'striped', 'outlined']
+shading = ['solid', 'striped', 'outline']
 
 //GLOBAL DOM VARIABLES
 
@@ -19,12 +19,14 @@ let gridSpace8 = document.getElementById('c8')
 let gridSpace9 = document.getElementById('c9')
 let gridSpace10 = document.getElementById('c10')
 let gridSpace11 = document.getElementById('c11')
-
+let reShuffleDeck = document.querySelector('.reShuffle')
+let getCardJS = document.querySelector('.getCard')
 //Game Grid Variable
 let gameGridJS = document.querySelector('.gameGrid')
+let addCard = []
 
 //Deck Variables
-const deck = []
+let deck = []
 //----------------------------
 
 //Player Class
@@ -49,9 +51,9 @@ class Card {
 //----------------------------
 
 //PlayerCreation
-for (let numPlayers = 0; numPlayers < 3; numPlayers++) {
-  let player = new Player()
-}
+// for (let numPlayers = 0; numPlayers < 3; numPlayers++) {
+//   let player = new Player()
+// }
 //----------------------------
 //Deck Array that uses a for loop to cycle through and create a deck of 81 unique cards and turns them into new card objects.
 for (let shapesIndex = 0; shapesIndex < shapes.length; shapesIndex++) {
@@ -74,29 +76,88 @@ for (let shapesIndex = 0; shapesIndex < shapes.length; shapesIndex++) {
   }
 }
 console.log(deck)
-
-//Function to add a new card to grid
-const addCardToGrid = () => {
-  return deck.shift
-}
 //----------------------------
 
-// Creation of a shape inside one of the grid cards with added properties for that specific card's shapes
-let cardProperty = document.createElement('div')
-gridSpace1.appendChild(cardProperty)
-cardProperty.className = 'blankShape outline square'
+// Test of creation of a shape inside one of the grid cards with added properties for that specific card's shapes
+// let cardProperty = document.createElement('div')
+// gridSpace1.appendChild(cardProperty)
+// cardProperty.className = 'blankShape greenStriped semiCircle'
+// let cardPropertyTwo = document.createElement('div')
+// gridSpace1.appendChild(cardPropertyTwo)
+// cardPropertyTwo.className = 'blankShape blueStriped square'
+// let cardPropertyThree = document.createElement('div')
+// gridSpace1.appendChild(cardPropertyThree)
+// cardPropertyThree.className = 'blankShape purpleStriped circle'
 //----------------------------
 
-// Attempt at Making Number of elements change on a grid item's card
-for (let i = 0; i < deck.length; i++) {
-  if (deck[i].number === 'One') {
-    let cardNumber = document.createElement('div')
-    deck[i].appendChild(cardNumber)
-  } else if (deck[i].number === 'Two') {
-    let cardNumber = document.createElement('div')
-    deck[i].appendChild(cardNumber)
-    let cardNumberTwo = document.createElement('div')
-    gridSpace1.appendChild(cardNumberTwo)
+// Attempt at making number of elements change on a grid item's card
+
+//----------------------------
+
+//Shuffle Function that mixes up the cards
+function shuffleDeck(deck) {
+  let newIndex = 0
+  let temp = null
+
+  for (let index = deck.length - 1; index > 0; index -= 1) {
+    newIndex = Math.floor(Math.random() * (index + 1))
+    temp = deck[index]
+    deck[index] = deck[newIndex]
+    deck[newIndex] = temp
   }
 }
+shuffleDeck(deck)
 //----------------------------
+
+//Function to get 'top' card in the deck
+function addCardToGrid() {
+  addCard = deck.shift()
+  addProperties()
+}
+//----------------------------
+
+//Set the properties of a newly gotten card to a space on the grid
+function addProperties() {
+  if (addCard.number === 'one') {
+    let cardNumber1 = document.createElement('div')
+    cardNumber1.className += 'blankShape '
+    cardNumber1.className += addCard.shape
+    gridSpace0.appendChild(cardNumber1)
+  } else if (addCard.number === 'two') {
+    let cardNumber1 = document.createElement('div')
+    cardNumber1.className += 'blankShape '
+    cardNumber1.className += addCard.shape
+    gridSpace0.appendChild(cardNumber1)
+    let cardNumber2 = document.createElement('div')
+    cardNumber2.className += 'blankShape '
+    cardNumber2.className += addCard.shape
+    gridSpace0.appendChild(cardNumber2)
+  } else if (addCard.number === 'three') {
+    let cardNumber1 = document.createElement('div')
+    cardNumber1.className += 'blankShape '
+    cardNumber1.className += addCard.shape
+    gridSpace0.appendChild(cardNumber1)
+    let cardNumber2 = document.createElement('div')
+    cardNumber2.className += 'blankShape '
+    cardNumber2.className += addCard.shape
+    gridSpace0.appendChild(cardNumber2)
+    let cardNumber3 = document.createElement('div')
+    cardNumber3.className += 'blankShape '
+    cardNumber3.className += addCard.shape
+    gridSpace0.appendChild(cardNumber3)
+  }
+}
+
+//EVENT LISTENERS
+reShuffleDeck.addEventListener('click', shuffleDeck)
+getCardJS.addEventListener('click', addCardToGrid)
+/* Game Process
+1. Have players insert names
+2. startGame Function run when start game BUTTON is pressed. 
+3. New player objects created with names and wonCounts of 0 to start.
+4.Create each card
+5. Create deck object holding every card
+6. Shuffle cards using ShuffleDeck function.
+8. Switch the page to the gameGrid page.
+7. Add 12 new cards to the gameGrid
+*/
